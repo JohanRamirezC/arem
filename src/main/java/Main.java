276c9541b9360f9ece5a023006b462af2e6f6291
Sebastian Castano <sebastian91ster@gmail.com>
2016-11-26 13:28:49 -0500

    primer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,25 +12,32 @@ import spark.ModelAndView;
 import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
+import org.apache.commons.validator.routines.CreditCardValidator;
 
 public class Main {
     static ArrayList<String> cedulas;
     static String verdad;
+    static CreditCardValidator validar;
   public static void main(String[] args) {
     cedulas = new ArrayList<String>();
+    validar=new CreditCardValidator(CreditCardValidator.AMEX+CreditCardValidator.VISA+CreditCardValidator.MASTERCARD+CreditCardValidator.DINERS);
         cedulas.add("1020758841");
         cedulas.add("41741678");
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-     get("/prueba/:name", (request, response) -> {
+     get("/prueba/:name/:credit", (request, response) -> {
            cedulas.forEach(cedula->{
                if(request.params(":name").equals(cedula)){
-                   verdad="existe";
+                   if(validar.isValid(request.params(":credit"))){
+                       verdad="El cliente puede comprar y tienen una tarjeta valida";
+                   }else{
+                       verdad="El cliente puede comprar pero no posee una tarjeta valida";
+                   }
                }
            });
            if(verdad==null){
-               verdad="no existe";
+               verdad="No puede comprar el cliente";
            }
             return "Hello: " + verdad;
         });
