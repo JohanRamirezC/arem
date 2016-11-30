c28fd03f8289e38ed628788368137fc3177df54e
Sebastian Castano <sebastian91ster@gmail.com>
2016-11-29 23:37:38 -0500

    Corregido problemas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,24 +16,8 @@ import org.apache.commons.validator.routines.CreditCardValidator;
 
 public class Main {
     static CreditCardValidator validar;
-    ArrayList<String> cedulas;
-     String verdad;
-    private String validar(ArrayList<String> cedulas,String name,String credit){
-        verdad="";
-        cedulas.forEach(cedula->{
-               if(name.equals(cedula)){
-                   if(validar.isValid(credit)){
-                      verdad="El cliente puede comprar y tienen una tarjeta valida";
-                   }else{
-                      verdad="El cliente puede comprar pero no posee una tarjeta valida";
-                   }
-               }
-           });
-           if(verdad==null){
-               verdad="No puede comprar el cliente";
-           }
-            return verdad;
-    }
+    static ArrayList<String> cedulas;
+    
   public static void main(String[] args) {
    ArrayList<String> cedulas = new ArrayList<String>();
     validar=new CreditCardValidator(CreditCardValidator.AMEX+CreditCardValidator.VISA+CreditCardValidator.MASTERCARD+CreditCardValidator.DINERS);
@@ -42,8 +26,20 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
      get("/validar/:name/:credit", (request, response) -> {
-           Main verificar=new Main();
-            return verificar.validar(cedulas, request.params(":name"), request.params(":credit"));
+         String v=null;
+           for(int i=0;i<cedulas.size();i++){
+               if(request.params(":name").equals(cedulas.get(i))){
+                   if(validar.isValid(request.params(":credit"))){
+                      v= "El cliente puede comprar y tienen una tarjeta valida";
+                   }else{
+                      v= "El cliente puede comprar pero no posee una tarjeta valida";
+                   }
+               }
+           }
+           if(v==null){
+               v="No puede comprar el cliente";
+           }
+            return v;
         });
 
   }
