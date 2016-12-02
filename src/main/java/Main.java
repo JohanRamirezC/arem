34e5df2f50e39a3b59d96c62e3a47df009608358
JohanRamirezC <johan.ramirez-c@mail.escuelaing.edu.co>
2016-12-02 11:18:58 -0500

    valores propios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,21 +15,36 @@ import com.heroku.sdk.jdbc.DatabaseUrl;
 import org.apache.commons.validator.routines.CreditCardValidator;
 
 public class Main {
-    static CreditCardValidator validar;
-    static ArrayList<String> cedulas;
-    
+    static ArrayList<String> personaEntrega;
+    static ArrayList<String> facturas;
+    static ArrayList<String> observacion;
+    static ArrayList<String> personaRecibe;
+ 
   public static void main(String[] args) {
-   ArrayList<String> cedulas = new ArrayList<String>();
+    ArrayList<String> facturas = new ArrayList<String>();
+    ArrayList<String> personaEntrega = new ArrayList<String>();
+    ArrayList<String> observacion = new ArrayList<String>();
+    ArrayList<String> personaRecibe = new ArrayList<String>();
    
-        cedulas.add("1020758841");
-        cedulas.add("41741678");
+    facturas.add("1020758841");
+    facturas.add("41741678");
+    personaEntrega.add("kevin sanchez");
+    personaEntrega.add("jose arboleda");
+    observacion.add("ninguna");
+    observacion.add("hay latas abolladas");
+    personaRecibe.add("alexander ceron manco");
+    personaRecibe.add("joseph arboleda");
+    
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
      get("/:name", (request, response) -> {
          String v=null;
-           for(int i=0;i<cedulas.size();i++){
-               if(request.params(":name").equals(cedulas.get(i))){
-                   v="lo logramos perro";
+           for(int i=0;i<facturas.size();i++){
+               if(request.params(":name").equals(facturas.get(i))){
+                   v="id factura "+facturas.get(i)
+                     +" persona que recibe : "+personaRecibe.get(i)
+                     +" persona que entrega : "+personaEntrega.get(i)
+                     +" observacion : "+observacion.get(i);
                }
            }
            if(v==null){
